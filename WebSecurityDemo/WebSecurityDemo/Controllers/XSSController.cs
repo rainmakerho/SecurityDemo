6aa05fcaf10610123d9d80dd789dc222ed34146e
rainmaker_ho <rainmaker_ho@msn.com>
2016-08-12 14:01:17 +0800

    新增加入XSS Controller 的測試 .NET 4.6.1

---
+++ WebSecurityDemo/WebSecurityDemo/Controllers/XSSController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSecurityDemo.Models;
+
+namespace WebSecurityDemo.Controllers
+{
+    public class XSSController : Controller
+    {
+        // GET: XSS
+        public ActionResult T1P1(string id)
+        {
+            var myProduct = new Product1(id);
+            return Json(myProduct, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult T2P1(string id)
+        {
+            var myProduct = new Product1(id);
+            var settings = new JsonSerializerSettings();
+            settings.StringEscapeHandling = StringEscapeHandling.EscapeHtml;
+            var encodeJsonString = JsonConvert.SerializeObject(myProduct, settings);
+            return this.Content(encodeJsonString, "application/json");
+        }
+
+        public ActionResult T3P2(string id)
+        {
+            var myProduct = new Product2(id);
+            return Json(myProduct, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebSecurityDemo/WebSecurityDemo/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSecurityDemo.Models
+{
+    public class Products
+    {
+        public Product1 P1 { get; set; }
+
+        public Product2 P2 { get; set; }
+    }
+
+    public class Product1
+    {
+        public string Id { get; set; }
+
+        public Product1(string id)
+        {
+            Id = "ProductId:" + id + "|<script>";
+        }
+    }
+
+    public class Product2
+    {
+        public string Id { get; set; }
+
+        public Product2(string id)
+        {
+            Id = $"ProductId:{id}|<script>";
+        }
+    }
+}
