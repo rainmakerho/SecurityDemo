07d2a6064fb0224ed5e34ed13d07d34ffa8b494c
rainmaker_ho <rainmaker_ho@msn.com>
2016-09-26 14:42:06 +0800

    add JsonFormatter @ global.asax.cs

--- WebSecurityDemo/WebSecurityDemo/Global.asax.cs
+++ WebSecurityDemo/WebSecurityDemo/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace WebSecurityDemo
 {
@@ -14,6 +15,12 @@ namespace WebSecurityDemo
     {
         void Application_Start(object sender, EventArgs e)
         {
+            //use StringEscapeHandling to encode Json(myComp, JsonRequestBehavior.AllowGet);
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.StringEscapeHandling = StringEscapeHandling.EscapeHtml;
+
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
